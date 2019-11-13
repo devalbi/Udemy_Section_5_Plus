417bd3780e3d03faaa40dcb5e1b591fb56c7c5a6
Arron Brady <abrady@techmatesgroup.com>
2019-11-13 14:08:11 -0500

    Palindrome Exe in Progress.

--- src/devalbi/udemy/exercises/Palindrome_4.java
+++ src/devalbi/udemy/exercises/Palindrome_4.java
@@ -3,6 +3,26 @@ package devalbi.udemy.exercises;
 public class Palindrome_4 {
     public static void main(String[] args)
     {
-
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(706));
     }
+
+    public static boolean isPalindrome(int number){
+        int reverse = 0;
+        int lastDigit = 0;
+
+        while(number > 0 ){
+            lastDigit = number % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            number /= 10;
+        }
+
+        System.out.println("Number is " + number + " and revered is " + reverse);
+        if(reverse == number){
+            return true;
+        } else return false;
+    }
+
+
 }
