91e8dfc898506ac65bddaba1805da314e722bb1b
Arron <arronlbrady@gmail.com>
2020-01-14 22:38:12 -0500

    Abstract class done. Minor change to ListClass

--- src/devalbi/udemy/section_9_abstraction/challenge/abstractclass/ListClass.java
+++ src/devalbi/udemy/section_9_abstraction/challenge/abstractclass/ListClass.java
@@ -2,11 +2,7 @@ package devalbi.udemy.section_9_abstraction.challenge.abstractclass;
 
 public class ListClass extends Node {
 
-    public ListClass() {
+    public ListClass(Object o) {
+        super(o);
     }
-
-
-    public
-
-
 }
