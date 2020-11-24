a18314777581ccc64aed516a339cc3e59f1f739b
Arron <arronlbrady@gmail.com>
2020-11-23 20:11:02 -0500

    Section 5 - exercise revision cntd...Starting NumberToWords

--- src/devalbi/udemy/Section_5/challenges/challnege_2/LastDigitChecker.java
+++ src/devalbi/udemy/Section_5/challenges/challnege_2/LastDigitChecker.java
@@ -34,20 +34,29 @@ public class LastDigitChecker {
     }
 
     public static boolean hasSameLastDigit(int num1, int num2, int num3) {
-        if((num1 < 10) || (num1 > 1000) || (num2 < 10) || (num2 > 1000) || (num3 < 10) || (num3 > 1000)) {
+        if((!isValid(num1) || !isValid(num1) || !isValid(num1))) {
             System.out.println("Number is outside of range, Error");
             return false;
         }
-        //TODO Add Logic
-        return true;
+
+        num1 %= 10;
+        num2 %= 10;
+        num3 %= 10;
+
+        if ( (num1 == num2) || (num2 == num3) || (num1 == num3 )) {
+            System.out.println("True");
+            return true;
+        }
+
+        System.out.println("False");
+        return false;
     }
 
     public static boolean isValid(int number) {
         if((number < 10) || (number > 1000)) {
-            System.out.println("Number is outside of range, Invalid");
             return false;
         }
         //TODO add Logic
-        return false;
+        return true;
     }
 }
