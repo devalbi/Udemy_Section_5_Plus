e48138a801885e2c9023aa2e55c6c79eb71043bb
Arron Brady <abrady@techmatesgroup.com>
2021-03-27 16:42:39 +0000

    Jar Package Challenge Complete

---
+++ src/devalbi/udemy/section_11_naming_con/challenge2/scope/Main.java
@@ -0,0 +1,12 @@
+package devalbi.udemy.section_11_naming_con.challenge2.scope;
+
+import com.company.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Series.nSum(10));
+        Series.factorial(10);
+        Series.fibonacci(10);
+    }
+}
