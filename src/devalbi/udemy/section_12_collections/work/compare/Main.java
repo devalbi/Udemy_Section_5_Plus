f6c70db55acafb1859d57f2f69ee332de84825eb
devalbi <arronlbrady@gmail.com>
2021-05-23 21:18:14 +0100

    Section 16 in progress, lamdas

--- src/devalbi/udemy/section_12_collections/work/compare/Main.java
+++ src/devalbi/udemy/section_12_collections/work/compare/Main.java
@@ -1,7 +1,6 @@
 package devalbi.udemy.section_12_collections.work.compare;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,7 +8,48 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Theatre theatre = new Theatre("Olympian", 8, 12);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Theatre theatre = new Theatre("Olympian", 8, 12);
+                // List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+
+
+                //reserving same seat for different arraylists (shallow copy here)
+                //Proves shallow list is a reference to the same lists
+                if(theatre.reserveSeat("D12")){
+                    System.out.println("Please Pay for D12");
+                } else {
+                    System.out.println("Seat is already reserved");
+                }
+
+                if(theatre.reserveSeat("D12")){
+                    System.out.println("Please Pay for D12");
+                } else {
+                    System.out.println("Seat is already reserved");
+                }
+
+                if(theatre.reserveSeat("B13")){
+                    System.out.println("Please Pay for B13");
+                } else {
+                    System.out.println("Seat is already reserved");
+                }
+
+                List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+                Collections.reverse(reverseSeats);
+                printList(reverseSeats);
+
+                List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+                priceSeats.add(theatre.new Seat("B00", 13.00));
+                priceSeats.add(theatre.new Seat("A00", 13.00));
+                Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+
+            }
+        });
+
+        thread.start();
+
+       /* Theatre theatre = new Theatre("Olympian", 8, 12);
        // List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
 
 
@@ -40,7 +80,7 @@ public class Main {
         List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
         priceSeats.add(theatre.new Seat("B00", 13.00));
         priceSeats.add(theatre.new Seat("A00", 13.00));
-        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);*/
 
     }
 
