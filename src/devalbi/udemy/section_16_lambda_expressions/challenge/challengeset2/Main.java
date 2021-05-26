4fc9d77b617e23f4d71ee346be552f852c3f4d8f
devalbi <arronlbrady@gmail.com>
2021-05-26 12:50:44 +0100

    Section 16 Challenge Set 2 started - in progress

--- src/devalbi/udemy/section_16_lambda_expressions/challenge/challengeset2/Main.java
+++ src/devalbi/udemy/section_16_lambda_expressions/challenge/challengeset2/Main.java
@@ -1,8 +1,24 @@
 package devalbi.udemy.section_16_lambda_expressions.challenge.challengeset2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
 
+        
     }
 }
