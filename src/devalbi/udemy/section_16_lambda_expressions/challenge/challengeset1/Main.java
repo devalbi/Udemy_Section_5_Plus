f04bb664484f7e295d7ae7e3c2259c239ea6ab68
devalbi <arronlbrady@gmail.com>
2021-05-26 12:06:00 +0100

    Section 16 Challenge Set 1 complete - tidied up

--- src/devalbi/udemy/section_16_lambda_expressions/challenge/challengeset1/Main.java
+++ src/devalbi/udemy/section_16_lambda_expressions/challenge/challengeset1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Challenge 1: Write as a Lambda
+//        Challenge 1: Write as a Lambda
 //        @Override
 //        public void run() {
 //            String myString = "Let's split this up into an array";
@@ -16,6 +16,7 @@ public class Main {
 //                System.out.println(part);
 //            }
 //        };
+
         String myString = "Let's split this up into an array";
         Runnable r = () -> {
             String myString1 = "Let's split this up into an array";
@@ -24,9 +25,10 @@ public class Main {
                 System.out.println(part);
             }
         };
-        // r.run();
 
-        // Challenge 2: Make into a lambda expression
+
+
+//        Challenge 2: Make into a lambda expression
 //        StringBuilder returnVal = new StringBuilder();
 //        for (int i = 0; i < s.length(); i++) {
 //            if (i % 2 == 1) {
@@ -46,29 +48,33 @@ public class Main {
             return returnVal.toString();
         };
 
+
+
         // Challenge 3: Make into a lambda that will execute the code with 123456789
         System.out.println(stringFunction.apply("1234567890"));
-
         System.out.println("====================================");
+
+
 
         //Challenge 5: Call the lamda from a method
         String functionTester = everySecondCharacter(stringFunction, "1234567890");
         System.out.println(functionTester);
-
-
         System.out.println("====================================");
+
+
 
         // Challenge 6: write lambda that uses Supplier interface to return a String
         // and assign result to String object iLoveJava
-
         Supplier<String> iLoveJava = () -> "I Love Java";
-        
+
+
+
         // Challenge 7: assign iLoveJva to variable and print to console
         String iLoveJavaVar = iLoveJava.get();
         System.out.println(iLoveJavaVar);
 
-        //
     }
+
 
     // Challenge 4: create method, using the function as an input param
     private static String everySecondCharacter(Function<String, String> stringFunction, String string) {
---
+++ src/devalbi/udemy/section_16_lambda_expressions/challenge/challengeset2/Main.java
@@ -0,0 +1,8 @@
+package devalbi.udemy.section_16_lambda_expressions.challenge.challengeset2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
